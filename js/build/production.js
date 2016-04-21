4328a3f9ab6572df1ec879faf23719185fd6a273
chrisboon <cboon27@gmail.com>
2016-04-21 12:05:43 -0400

    Removed complex sidebar 'memory'
    Worked but it caused a slight flicker of bad layout on page load.
    One solution would be to add a transition (probably opacity) to the page on load to resolve it but that seemed a hack.
    
    Signed-off-by: chrisboon <cboon27@gmail.com>

--- js/build/production.js
+++ js/build/production.js
@@ -258,62 +258,73 @@ $(document).ready(function(){
 
   //autorun on all pages
 
+	// var sidebar = {
+
+	// 	key: productIdentifier + 'SidebarOpen',
+
+	// 	canHas: false,
+
+	// 	toggleState: function(){
+	// 		var current = localStorage[this.key];
+	// 		if( current === "open"){
+	// 			$els.body.addClass("-js-sidebar-closed");
+	// 			localStorage[this.key] = "closed";
+	// 			console.log(localStorage[this.key]);
+	// 		}
+	// 		else if( current === "closed"){
+	// 			$els.body.removeClass("-js-sidebar-closed");
+	// 			localStorage[this.key] = "open";
+	// 			console.log(localStorage[this.key]);
+	// 		}
+	// 		else{
+	// 			console.log("sidebar.setState only accepts 'open' or 'closed' as options");
+	// 		}
+	// 	},
+	// 	setStateTrue: function(){
+	// 		$els.body.removeClass("-js-sidebar-closed");
+	// 		localStorage[this.key] = 'open';
+	// 	},
+	// 	setStateFalse: function(){
+	// 		$els.body.addClass("-js-sidebar-closed");
+	// 		localStorage[this.key] = 'closed';
+	// 	},
+
+	// 	getState: function(){
+	// 		return localStorage[this.key];
+	// 	},
+	// 	init: function(){
+	// 		if ($els.body.data("suppresssidebar") == true) {
+	// 			this.setStateFalse();
+	// 		}
+	// 		if (window.localStorage) { // checks if browser support localStorage
+	// 			this.canHas = true;
+				
+	// 			if (localStorage[this.key]) { // checks if value exists
+	// 				var setAs = this.getState();
+	// 				if (setAs ==='closed') {
+	// 					this.setStateFalse();
+	// 				}
+	// 			}
+	// 			else{
+	// 				this.setStateTrue();
+	// 			}
+	// 		}
+	// 		//if there is no localStorage we can't save state so may as well hide the close option
+	// 		else {
+	// 			$els.sidebarToggle.hide();
+	// 			console.log("localStorage is not supported");
+	// 		}
+
+	// 	}
+	// }
+
 	var sidebar = {
 
-		key: productIdentifier + 'SidebarOpen',
-
-		canHas: false,
 
 		toggleState: function(){
-			var current = localStorage[this.key];
-			if( current === "open"){
-				$els.body.addClass("-js-sidebar-closed");
-				localStorage[this.key] = "closed";
-				console.log(localStorage[this.key]);
-			}
-			else if( current === "closed"){
-				$els.body.removeClass("-js-sidebar-closed");
-				localStorage[this.key] = "open";
-				console.log(localStorage[this.key]);
-			}
-			else{
-				console.log("sidebar.setState only accepts 'open' or 'closed' as options");
-			}
-		},
-		setStateTrue: function(){
-			$els.body.removeClass("-js-sidebar-closed");
-			localStorage[this.key] = 'open';
-		},
-		setStateFalse: function(){
-			$els.body.addClass("-js-sidebar-closed");
-			localStorage[this.key] = 'closed';
-		},
-
-		getState: function(){
-			return localStorage[this.key];
+			$els.body.toggleClass("-js-sidebar-closed");
 		},
 		init: function(){
-			if ($els.body.data("suppresssidebar") == true) {
-				this.setStateFalse();
-			}
-			if (window.localStorage) { // checks if browser support localStorage
-				this.canHas = true;
-				
-				if (localStorage[this.key]) { // checks if value exists
-					var setAs = this.getState();
-					if (setAs ==='closed') {
-						this.setStateFalse();
-					}
-				}
-				else{
-					this.setStateTrue();
-				}
-			}
-			//if there is no localStorage we can't save state so may as well hide the close option
-			else {
-				$els.sidebarToggle.hide();
-				console.log("localStorage is not supported");
-			}
 
 		}
 	}
